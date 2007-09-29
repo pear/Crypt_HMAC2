0ad131e1103d1545d477e9b6ee7b8bb3a769fdf7
Pádraic Brady <padraic@php.net>
2007-09-29 12:12:09 +0000

    HMAC2 initial commit for OpenID ancillary support
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_HMAC2/trunk@243145 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+}
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'TestHelper.php';
+require_once 'HMAC2Test.php';
+
+class AllTests
+{
+    public static function main()
+    {
+        $parameters = array();
+
+        if (TESTS_GENERATE_REPORT && extension_loaded('xdebug')) {
+            $parameters['reportDirectory'] = TESTS_GENERATE_REPORT_TARGET;
+        }
+        PHPUnit_TextUI_TestRunner::run(self::suite(), $parameters);
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('PEAR - Crypt_HMAC2');
+
+        $suite->addTestSuite('Crypt_HMAC2Test');
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'AllTests::main') {
+    AllTests::main();
+}
