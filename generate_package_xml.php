b04d05c6e16a1019f141338ef149130436b5192b
Pádraic Brady <padraic@php.net>
2007-09-29 14:45:40 +0000

    package file updates post-release
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_HMAC2/trunk@243161 c90b9560-bf6c-de11-be94-00142212c4b1

--- generate_package_xml.php
+++ generate_package_xml.php
@@ -1,43 +1,58 @@
 <?php
 require_once('PEAR/PackageFileManager2.php');
 PEAR::setErrorHandling(PEAR_ERROR_DIE);
-//require_once 'PEAR/Config.php';
-//PEAR_Config::singleton('/path/to/unusualpearconfig.ini');
-// use the above lines if the channel information is not validating
-$packagexml = new PEAR_PackageFileManager2;
-// for an existing package.xml use
-// $packagexml = {@link importOptions()} instead
-$e = $packagexml->setOptions(
-    array('baseinstalldir' => 'Crypt',
-     'packagedirectory' => 'D:/xampp/htdocs/projects/pear/trunk/Crypt_HMAC2/',
-     'filelistgenerator' => 'file',
-     'dir_roles' => array('docs' => 'doc', 'tests' => 'test'),
-     'ignore' => array('generate_package_xml.php', '.svn')
-    )
+
+$packagefile = './package.xml';
+
+$options = array(
+    'filelistgenerator' => 'cvs',
+    'changelogoldtonew' => false,
+    'simpleoutput'      => true,
+    'baseinstalldir'    => '/',
+    'packagedirectory'  => './',
+    'packagefile'       => $packagefile,
+    'clearcontents'     => true,
+    'ignore'            => array('generate_package_xml.php', '.svn', '.cvs*'),
+    'dir_roles'         => array(
+        'docs'     => 'doc',
+        'examples' => 'doc',
+        'tests'    => 'test',
+    ),
 );
+
+$packagexml = &PEAR_PackageFileManager2::importOptions($packagefile, $options);
+$packagexml->setPackageType('php');
+
 $packagexml->setPackage('Crypt_HMAC2');
 $packagexml->setSummary('Implementation of Hashed Message Authentication Code for PHP5');
 $packagexml->setDescription("Implementation of Hashed Message Authentication Code for PHP5.\nThis package may use the hash or mhash extensions when enabled to\nextend the range of cryptographic hash functions beyond the natively\nimplemented MD5 and SHA1.");
+
 $packagexml->setChannel('pear.php.net');
-$packagexml->setAPIVersion('0.1.0');
-$packagexml->setReleaseVersion('0.1.0a3');
-$packagexml->setReleaseStability('alpha');
-$packagexml->setAPIStability('alpha');
-$packagexml->setNotes("* This is the proposal version...");
-$packagexml->setPackageType('php');
+
+$notes = <<<EOT
+* Initial release!
+* Updated tests location inside directory hierarchy for easier running
+* Incorporates all proposal comments or conditional notes
+EOT;
+$packagexml->setNotes($notes);
+
 $packagexml->setPhpDep('5.0.0');
-$packagexml->setPearinstallerDep('1.4.0');
+$packagexml->setPearinstallerDep('1.4.0b1');
+$packagexml->addPackageDepWithChannel('required', 'PEAR', 'pear.php.net', '1.3.6');
+
 $packagexml->addMaintainer('lead', 'padraic', 'Pádraic Brady', 'padraic@php.net');
 $packagexml->setLicense('New BSD License', 'http://opensource.org/licenses/bsd-license.php');
+
+$packagexml->addRelease();
 $packagexml->generateContents();
 
-//$pkg = &$packagexml->exportCompatiblePackageFile1(); // get a PEAR_PackageFile object
+$packagexml->setAPIVersion('0.2.0');
+$packagexml->setReleaseVersion('0.2.0');
+$packagexml->setReleaseStability('beta');
+$packagexml->setAPIStability('beta');
 
 if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
-    //$pkg->writePackageFile();
     $packagexml->writePackageFile();
 } else {
-    //$pkg->debugPackageFile();
     $packagexml->debugPackageFile();
 }
-?>
