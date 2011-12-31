c092837eaafbf21f2e25c9b5450fd7ad1f6c9924
Daniel Convissor <danielc@php.net>
2011-12-31 00:08:11 +0000

    Upgrade PHPUnit require statements (for PEAR QA Team).
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_HMAC2/trunk@321596 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -4,10 +4,17 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Crypt_HMAC2_AllTests::main');
 }
 
-require_once 'PHPUnit/TextUI/TestRunner.php';
+if ($fp = @fopen('PHPUnit/Autoload.php', 'r', true)) {
+    require_once 'PHPUnit/Autoload.php';
+} elseif ($fp = @fopen('PHPUnit/Framework.php', 'r', true)) {
+    require_once 'PHPUnit/Framework.php';
+    require_once 'PHPUnit/TextUI/TestRunner.php';
+} else {
+    die('skip could not find PHPUnit');
+}
+fclose($fp);
 
-require_once 'TestHelper.php';
-require_once 'HMAC2Test.php';
+require_once dirname(__FILE__) . '/HMAC2Test.php';
 
 class Crypt_HMAC2_AllTests
 {
